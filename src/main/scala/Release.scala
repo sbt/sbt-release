8938950bb10047b47a8521c34469d81c9ed4f4bd
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-10-15 21:16:04 +0200

    Move Version into a separate source file.

--- src/main/scala/Release.scala
+++ src/main/scala/Release.scala
@@ -101,32 +101,3 @@ object Release {
   }
 
 }
-
-
-private[sbtrelease] object Version {
-  val VersionR = """([0-9]+)(?:(?:\.([0-9]+))?(?:\.([0-9]+))?)?(-.*)?""".r
-
-  def apply(s: String): Option[Version] = {
-    allCatch opt {
-      val VersionR(maj, min, mic, qual) = s
-      Version(maj.toInt, Option(min).map(_.toInt), Option(mic).map(_.toInt), Option(qual))
-    }
-  }
-}
-
-private[sbtrelease] case class Version(major: Int, minor: Option[Int], micro: Option[Int], qualifier: Option[String]) {
-  def bump = {
-    val maybeBumpedMicro = micro.map(m => copy(micro = Some(m + 1)))
-    val maybeBumpedMinor = minor.map(m => copy(minor = Some(m + 1)))
-    lazy val bumpedMajor = copy(major = major + 1)
-
-    maybeBumpedMicro.orElse(maybeBumpedMinor).getOrElse(bumpedMajor)
-  }
-
-  def withoutQualifier = copy(qualifier = None)
-  def asSnapshot = copy(qualifier = Some("-SNAPSHOT"))
-
-  def string = "" + major + get(minor) + get(micro) + qualifier.getOrElse("")
-
-  private def get(part: Option[Int]) = part.map("." + _).getOrElse("")
-}
