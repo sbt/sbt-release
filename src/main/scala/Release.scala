7d3f009cea6d467c23e3039094a0a2b0b62b27f4
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-10-16 13:54:00 +0200

    Initial simple release process modeled as commands.

--- src/main/scala/Release.scala
+++ src/main/scala/Release.scala
@@ -2,76 +2,98 @@ package sbtrelease
 
 import sbt._
 import Keys._
+import complete.DefaultParsers._
 import util.control.Exception._
 
 object Release {
 
   lazy val snapshotDependencies = TaskKey[Seq[ModuleID]]("release-snapshot-dependencies")
-  lazy val checkSnapshotDependencies = TaskKey[Boolean]("release-check-snapshot-dependencies")
+
+  lazy val useDefaults = SettingKey[Boolean]("release-use-defaults")
 
   type Versions = (String, String)
   lazy val versions = SettingKey[Versions]("release-versions")
   lazy val inquireVersions = TaskKey[Versions]("release-inquire-versions")
 
-  lazy val useDefaults = TaskKey[Boolean]("release-use-defaults")
-
-  lazy val release = TaskKey[Unit]("release")
   lazy val releaseStage2 = TaskKey[Unit]("release-stage-2")
 
 
 
-  lazy val settings = Seq[Setting[_]](
+  val releaseCommand = "release"
+  val releaseParser = (Space ~> "with-defaults")?
+  val release: Command = Command(releaseCommand)(_ => releaseParser) { (st, switch) =>
+    val extracted = Project.extract(st)
+    val ref = extracted.get(thisProjectRef)
 
+    import Project.showFullKey
+
+    Seq(
+      checkSnapshotDependenciesCommand,
+      showFullKey(inquireVersions in Global in ref),
+      showFullKey(test in Test in ref),
+      setReleaseVersionCommand,
+      showFullKey(test in Test in ref),
+      showFullKey(publishLocal in Global in ref),
+      setNextVersionCommand
+    ) ::: Project.extract(st).append(Seq(useDefaults := switch.isDefined), st)
+  }
+
+  val checkSnapshotDependenciesCommand = "release-check-snapshot-dependencies"
+  val checkSnapshotDependencies = Command.command(checkSnapshotDependenciesCommand) { (st) =>
+    val extracted = Project.extract(st)
+    val (newSt, snapshotDeps) = extracted.runTask(snapshotDependencies, st)
+    val useDefs = extracted.get(useDefaults)
+    if (!snapshotDeps.isEmpty) {
+      if (useDefs) {
+        sys.error("Aborting release due to snapshot dependencies.")
+      } else {
+        CommandSupport.logger(newSt).warn("Snapshot dependencies detected:\n" + snapshotDeps.mkString("\n"))
+        SimpleReader.readLine("Do you want to continue (y/n)? [n] ") match {
+          case Some("y") | Some("Y") =>
+          case _ => sys.error("Aborting release due to snapshot dependencies.")
+        }
+      }
+    }
+    newSt
+  }
+
+  val setReleaseVersionCommand = "release-set-release-version"
+  val setReleaseVersion = setVersionCommand(setReleaseVersionCommand, _._1)
+
+  val setNextVersionCommand = "release-set-next-version"
+  val setNextVersion = setVersionCommand(setNextVersionCommand, _._2)
+
+
+  def setVersionCommand(key: String, selectVersion: Versions => String) = Command.command(key) { st =>
+    val extracted = Project.extract(st)
+    val vs = extracted.get(versions)
+    val selected = selectVersion(vs)
+
+    CommandSupport.logger(st).info("Setting version to '%s'." format selected)
+
+    extracted.append(Seq(
+      version := selected,
+      versions := vs
+    ), st)
+  }
+
+
+  lazy val settings = Seq[Setting[_]](
+    useDefaults := false,
+  
     snapshotDependencies <<= (fullClasspath in Runtime) map { cp: Classpath =>
       val moduleIds = cp.flatMap(_.get(moduleID.key))
       val snapshots = moduleIds.filter(m => m.isChanging || m.revision.endsWith("-SNAPSHOT"))
       snapshots
     },
 
-    checkSnapshotDependencies <<= (snapshotDependencies, streams) map ( (snapshots, s) =>
-      if (!snapshots.isEmpty) {
-        s.log.warn("Snapshot dependencies detected:" + snapshots.mkString("\n"))
-        SimpleReader.readLine("Do you want to continue (y/n)? [n] ") match {
-          case Some("y") | Some("Y") => true
-          case _ => false
-        }
-      } else
-        true
-      ),
+    versions <<= version(ver => Version(ver).map(
+      v => (v.withoutQualifier.string, v.bump.asSnapshot.string)).getOrElse(versionFormatError)
+    ),
 
-    loadOrSetVersions,
     inquireVersionsTask,
 
-    release <<= (checkSnapshotDependencies, inquireVersions.task) flatMap {
-      (continueWithSnapshotDeps, inquire) =>
-        if (continueWithSnapshotDeps) inquire map ( _ => ())
-        else sys.error("Aborting release with snapshot dependencies!")
-    },
-
-    // executes the tests, set the version to the release-version and let release2 run after reapplying the settings.
-    release <<= (release, test.task in Test) flatMap {
-      (_, testTask) => testTask
-    } updateState { case (st, _) =>
-      val extracted = Project.extract(st)
-      val (releaseV, nextV) = extracted.get(versions)
-      val ref = extracted.get(thisProjectRef)
-        Project.showFullKey(releaseStage2 in Global in ref) :: extracted.append(Seq(
-        // set the version in the next stage to the release version
-        version := releaseV,
-        // carry over release-version and next-version, otherwise they would get reset by the 'versions <== ...' statement
-        versions := (releaseV, nextV)
-      ), st)
-    },
-
-    // releaseStage2 is run after the version has been set to the release-version in the release task.
-    // using publish-local for now.
-    releaseStage2 <<= (test in Test, publishLocal.task) flatMap {
-      (_, publishTask) => publishTask
-    } updateState { case (st, _) =>
-      val extracted = Project.extract(st)
-      val (_, nextV) = extracted.get(versions)
-      extracted.append(Seq(version := nextV), st)
-    }
+    commands ++= Seq(release, checkSnapshotDependencies, setReleaseVersion, setNextVersion)
 
   )
 
@@ -84,24 +106,20 @@ object Release {
   }
 
   def inquireVersionsTask =
-    inquireVersions <<= (versions, streams).map({ case ((releaseVersion, nextVersion), s) =>
-      val releaseV = readVersion(releaseVersion, "Release version [%s] : " format releaseVersion)
-      val nextV = readVersion(nextVersion, "Next version [%s] : " format nextVersion)
+    inquireVersions <<= (useDefaults, versions, streams).map({ case (useDefs, (releaseVersion, nextVersion), s) =>
+      val releaseV =
+        if (useDefs) releaseVersion
+        else readVersion(releaseVersion, "Release version [%s] : " format releaseVersion)
+
+      val nextV =
+        if (useDefs) nextVersion
+        else readVersion(nextVersion, "Next version [%s] : " format nextVersion)
+
       (releaseV, nextV)
     }).updateState { case (st, vs) =>
       val extracted = Project.extract(st)
       extracted.append(Seq(versions := vs), st)
     }
 
-  def loadOrSetVersions = versions <<= version(ver => Version(ver).map(
-    v => (v.withoutQualifier.string, v.bump.asSnapshot.string)).getOrElse(versionFormatError)
-  )
-
   private def versionFormatError = sys.error("Version format is not compatible with [0-9]+([0-9]+)?([0-9]+)?(-.*)?")
-
-  def clearKey[T](key: ScopedKey[Task[T]], state: State): State = {
-    import SessionVar._
-    state.update(sessionVars)(om => Map(orEmpty(om).map.remove (Key(key))))
-  }
-
 }
