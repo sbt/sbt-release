3e6516556c3e5041d4803d097c44893afa22c4fc
Andrew Gustafson <gus4000@gmail.com>
2013-07-21 00:43:05 +0100

    Add setting to control version bump

--- src/main/scala/Version.scala
+++ src/main/scala/Version.scala
@@ -3,6 +3,19 @@ package sbtrelease
 import util.control.Exception._
 
 object Version {
+  sealed trait Bump {
+    def bump: Version => Version
+  }
+
+  object Bump {
+    object Major extends Bump { def bump = _.bumpMajor }
+    object Minor extends Bump { def bump = _.bumpMinor }
+    object Bugfix extends Bump { def bump = _.bumpBugfix }
+    object Next extends Bump { def bump = _.bump }
+
+    val default = Minor
+  }
+
   val VersionR = """([0-9]+)(?:(?:\.([0-9]+))?(?:\.([0-9]+))?)?([\-0-9a-zA-Z]*)?""".r
 
   def apply(s: String): Option[Version] = {
@@ -25,6 +38,8 @@ case class Version(major: Int, minor: Option[Int], bugfix: Option[Int], qualifie
   def bumpMajor = copy(major = major + 1, minor = minor.map(_ => 0), bugfix = bugfix.map(_ => 0))
   def bumpMinor = copy(minor = minor.map(_ + 1), bugfix = bugfix.map(_ => 0))
   def bumpBugfix = copy(bugfix = bugfix.map(_ + 1))
+
+  def bump(bumpType: Version.Bump): Version = bumpType.bump(this)
 
   def withoutQualifier = copy(qualifier = None)
   def asSnapshot = copy(qualifier = Some("-SNAPSHOT"))
