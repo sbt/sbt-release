51e9b7985af030bbb66b4c8f0b2c7d172d61137b
timvlaer <timvanlaer@gmail.com>
2014-04-17 14:49:10 +0200

    code improvements

--- src/main/scala/Vcs.scala
+++ src/main/scala/Vcs.scala
@@ -153,17 +153,13 @@ class Subversion(val baseDir: File) extends Vcs {
   override def cmd(args: Any*): ProcessBuilder = Process(exec +: args.map(_.toString), baseDir)
 
   override def add(files: String*) = {
-    var filesToAdd:Set[String] = Set()
-    files.foreach(f => {
-      val isFileUnderVersionControl = Try(cmd("info", f).!!).isSuccess
-      if(!isFileUnderVersionControl) filesToAdd += f
-    })
-    if(!filesToAdd.isEmpty) cmd(("add" +: filesToAdd.toSeq): _*) else noop
+    val filesToAdd = files.filterNot(isFileUnderVersionControl)
+    if(!filesToAdd.isEmpty) cmd(("add" +: filesToAdd): _*) else noop
   }
 
   override def commit(message: String) = cmd("commit", "-m", message)
 
-  override def currentBranch: String = getWorkingDirSvnUrl.substring(getWorkingDirSvnUrl.lastIndexOf("/") + 1)
+  override def currentBranch: String = workingDirSvnUrl.substring(workingDirSvnUrl.lastIndexOf("/") + 1)
 
   override def pushChanges: ProcessBuilder = commit("push changes")
 
@@ -174,41 +170,45 @@ class Subversion(val baseDir: File) extends Vcs {
   override def hasUpstream: Boolean = true
 
   override def tag(name: String, comment: String, force: Boolean): ProcessBuilder = {
-    val tagUrl = getRepoRoot + "tags/" + name
+    val tagUrl = getSvnTagUrl(name)
     if(force && existsTag(name)) {
       val deleteTagComment = comment + ", \ndelete tag " + name + " to create a new one."
       cmd("del", tagUrl, "-m", deleteTagComment).!!
     }
-    cmd("copy", getWorkingDirSvnUrl, tagUrl, "-m", comment)
+    cmd("copy", workingDirSvnUrl, tagUrl, "-m", comment)
   }
 
   override def checkRemote(remote: String): ProcessBuilder = noop
 
   override def existsTag(name: String): Boolean = {
-    val tagUrl = getRepoRoot + "tags/" + name
-    Try(cmd("info", tagUrl).!!).isSuccess
+    Try(cmd("info", getSvnTagUrl(name)).!!).isSuccess
   }
 
   override def currentHash: String = ""
 
   override def status: ProcessBuilder = cmd("status", "-q")
 
-  private def getWorkingDirSvnUrl:String = {
+  lazy val workingDirSvnUrl:String = {
     val svnInfo = cmd("info").!!
-    val urlStartIdx = svnInfo.indexOf("URL: ") + 5
+    val svnInfoUrlKey = "URL: "
+    val urlStartIdx = svnInfo.indexOf(svnInfoUrlKey) + svnInfoUrlKey.length
     svnInfo.substring(urlStartIdx, svnInfo.indexOf('\n', urlStartIdx)-1)
   }
 
-  private def getRepoRoot:String = {
+  lazy val repoRoot:String = {
     val svnBaseUrlEndIdxOptions = List(
-      getWorkingDirSvnUrl.indexOf("/trunk"),
-      getWorkingDirSvnUrl.indexOf("/branches"),
-      getWorkingDirSvnUrl.indexOf("/tags")
+      workingDirSvnUrl.indexOf("/trunk"),
+      workingDirSvnUrl.indexOf("/branches"),
+      workingDirSvnUrl.indexOf("/tags")
     ).filter(_ >= 0)
     require(!svnBaseUrlEndIdxOptions.isEmpty, "No /trunk, /branches or /tags part found in svn url. Base url cannot be extracted.")
     val svnBaseUrlEndIdx = svnBaseUrlEndIdxOptions.head
-    getWorkingDirSvnUrl.substring(0, svnBaseUrlEndIdx + 1)
+    workingDirSvnUrl.substring(0, svnBaseUrlEndIdx + 1)
   }
+
+  private def getSvnTagUrl(name: String): String = repoRoot + "tags/" + name
+
+  private def isFileUnderVersionControl(file: String): Boolean = Try(cmd("info", file).!!).isSuccess
 
   private def noop:ProcessBuilder = status
 }
