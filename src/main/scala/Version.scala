2da48e4ff02b19bdea8402b190dbca8a0b79e18b
kenji yoshida <6b656e6a69@gmail.com>
2023-12-28 21:05:34 +0900

    Revert "feat: support prerelease version increments (#393)"
    This reverts commit 5fea9d8657b69949c6481786d3d9a5da09f9a3ef.

--- src/main/scala/Version.scala
+++ src/main/scala/Version.scala
@@ -8,36 +8,11 @@ object Version {
   }
 
   object Bump {
-
-    /**
-     * Strategy to always bump the major version by default. Ex. 1.0.0 would be bumped to 2.0.0
-     */
-    case object Major extends Bump { def bump: Version => Version = _.bumpMajor }
-    /**
-     * Strategy to always bump the minor version by default. Ex. 1.0.0 would be bumped to 1.1.0
-     */
-    case object Minor extends Bump { def bump: Version => Version = _.bumpMinor }
-    /**
-     * Strategy to always bump the bugfix version by default. Ex. 1.0.0 would be bumped to 1.0.1
-     */
-    case object Bugfix extends Bump { def bump: Version => Version = _.bumpBugfix }
-    /**
-     * Strategy to always bump the nano version by default. Ex. 1.0.0.0 would be bumped to 1.0.0.1
-     */
-    case object Nano extends Bump { def bump: Version => Version = _.bumpNano }
-
-    case object Next extends Bump { def bump: Version => Version = _.bump }
-    /**
-     * Strategy to always increment to the next version from smallest to greatest
-     * Ex:
-     * Major: 1 becomes 2
-     * Minor: 1.0 becomes 1.1
-     * Bugfix: 1.0.0 becomes 1.0.1
-     * Nano: 1.0.0.0 becomes 1.0.0.1
-     * Qualifier with version number: 1.0-RC1 becomes 1.0-RC2
-     * Qualifier without version number: 1.0-alpha becomes 1.0
-     */
-    case object NextWithQualifier extends Bump { def bump: Version => Version = _.bumpWithQualifier }
+    case object Major extends Bump { def bump = _.bumpMajor }
+    case object Minor extends Bump { def bump = _.bumpMinor }
+    case object Bugfix extends Bump { def bump = _.bumpBugfix }
+    case object Nano extends Bump { def bump = _.bumpNano }
+    case object Next extends Bump { def bump = _.bump }
 
     val default = Next
   }
@@ -71,30 +46,6 @@ case class Version(major: Int, subversions: Seq[Int], qualifier: Option[String])
       .getOrElse(bumpedMajor)
   }
 
-  def bumpWithQualifier = {
-    val maybeBumpedPrerelease = qualifier.collect {
-      case rawQualifier @ Version.PreReleaseQualifierR() =>
-        val qualifierEndsWithNumberRegex = """[0-9]*$""".r
-
-        val opt = for {
-          versionNumberQualifierStr <- qualifierEndsWithNumberRegex.findFirstIn(rawQualifier)
-          versionNumber <- Try(versionNumberQualifierStr.toInt)
-            .toRight(new Exception(s"Version number not parseable to a number. Version number received: $versionNumberQualifierStr"))
-            .toOption
-          newVersionNumber = versionNumber + 1
-          newQualifier = rawQualifier.replaceFirst(versionNumberQualifierStr, newVersionNumber.toString)
-        } yield Version(major, subversions, Some(newQualifier))
-
-        opt.getOrElse(copy(qualifier = None))
-    }
-    def maybeBumpedLastSubversion = bumpSubversionOpt(subversions.length-1)
-    def bumpedMajor = copy(major = major + 1)
-
-    maybeBumpedPrerelease
-      .orElse(maybeBumpedLastSubversion)
-      .getOrElse(bumpedMajor)
-  }
-
   def bumpMajor  = copy(major = major + 1, subversions = Seq.fill(subversions.length)(0))
   def bumpMinor  = maybeBumpSubversion(0)
   def bumpBugfix = maybeBumpSubversion(1)
@@ -114,19 +65,7 @@ case class Version(major: Int, subversions: Seq[Int], qualifier: Option[String])
   def bump(bumpType: Version.Bump): Version = bumpType.bump(this)
 
   def withoutQualifier = copy(qualifier = None)
-  def asSnapshot = copy(qualifier = qualifier.map { qualifierStr =>
-    s"$qualifierStr-SNAPSHOT"
-  }.orElse(Some("-SNAPSHOT")))
-
-  def withoutSnapshot: Version = copy(qualifier = qualifier.flatMap { qualifierStr =>
-    val snapshotRegex = """-SNAPSHOT""".r
-    val newQualifier = snapshotRegex.replaceFirstIn(qualifierStr, "")
-    if (newQualifier == qualifierStr) {
-      None
-    } else {
-      Some(newQualifier)
-    }
-  })
+  def asSnapshot = copy(qualifier = Some("-SNAPSHOT"))
 
   def string = "" + major + mkString(subversions) + qualifier.getOrElse("")
 
