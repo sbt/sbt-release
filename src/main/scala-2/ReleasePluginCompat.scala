2230b87a726a048a37b625eb9d8d023deb505369
xuwei-k <6b656e6a69@gmail.com>
2026-01-10 15:43:12 +0900

    run cleanFull if sbt 2

--- src/main/scala-2/ReleasePluginCompat.scala
+++ src/main/scala-2/ReleasePluginCompat.scala
@@ -2,9 +2,18 @@ package sbtrelease
 
 import sbt.*
 import sbt.Keys.*
+import sbtrelease.ReleasePlugin.autoImport.ReleaseStep
 
 private[sbtrelease] object ReleasePluginCompat {
   def testTask: TaskKey[?] = sbt.Keys.test
+
+  val runClean: ReleaseStep = ReleaseStep(
+    action = { st =>
+      val extracted = Project.extract(st)
+      val ref = extracted.get(thisProjectRef)
+      extracted.runAggregated(ref / (Global / clean), st)
+    }
+  )
 
   val moduleIds: Def.Initialize[Task[Seq[ModuleID]]] = Def.task(
     (Runtime / managedClasspath).value.flatMap(_.get(moduleID.key))
--- src/main/scala-3/ReleasePluginCompat.scala
+++ src/main/scala-3/ReleasePluginCompat.scala
@@ -2,9 +2,13 @@ package sbtrelease
 
 import sbt.*
 import sbt.Keys.*
+import sbtrelease.ReleasePlugin.autoImport.ReleaseStep
+import sbtrelease.ReleasePlugin.autoImport.releaseStepCommandAndRemaining
 
 private[sbtrelease] object ReleasePluginCompat {
   def testTask: TaskKey[?] = sbt.Keys.testFull
+
+  val runClean: ReleaseStep = releaseStepCommandAndRemaining(BasicCommandStrings.CleanFull)
 
   val moduleIds: Def.Initialize[Task[Seq[ModuleID]]] = Def.task(
     (Runtime / managedClasspath).value.flatMap(_.get(Keys.moduleIDStr)).map(Classpaths.moduleIdJsonKeyFormat.read)
